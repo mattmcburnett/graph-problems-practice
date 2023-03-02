95fe340f4bdd0fb8f96cfdbc913a1182491251d6
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-03-02 18:53:49 -0500

    finished island size

--- island.js
+++ island.js
@@ -1,13 +1,24 @@
 function getNeighbors(row, col, graph) {
+  const neighbors = [];
 
   // Check top
 
+  if(row>0 && graph[row-1][col] === 1){
+    neighbors.push([row-1, col]);
+  }
   // Check bottom
-
+  if(row<graph.length-1 && graph[row+1][col] === 1){
+    neighbors.push([row+1, col]);
+  }
   // Check left
-
+  if(col>0 && graph[row][col-1] === 1){
+    neighbors.push([row, col-1]);
+  }
   // Check right
-
+  if(col<graph[row].length-1 && graph[row][col+1] === 1){
+    neighbors.push([row, col+1]);
+  }
+  return neighbors;
   // Return neighbors
 
   // Your code here
@@ -17,15 +28,28 @@ function getNeighbors(row, col, graph) {
 function islandSize(row, col, graph) {
 
   // Create a visited set to store visited nodes
-
+  const visited = new Set([[row, col].toString()]);
   // Create a stack, put the starting node in the stack
-
+  const stack = [[row, col]];
   // Put the stringified starting node in visited
 
   // Initialize size to 0
-
+  let size = 0;
   // While the stack is not empty,
+  while(stack.length){
 
+    let current = stack.pop();
+    size++;
+    let neighbors = getNeighbors(current[0], current[1], graph);
+    neighbors.forEach(neighbor => {
+      if(!visited.has(neighbor.toString())){
+        visited.add(neighbor.toString());
+        stack.push(neighbor);
+      }
+
+    })
+
+  }
     // Pop the first node
 
     // DO THE THING (increment size by 1)
@@ -36,7 +60,7 @@ function islandSize(row, col, graph) {
     // HINT: Remember, you're storing your visited nodes as strings!
 
   // return size
-
+  return size;
   // Your code here
 }
 
